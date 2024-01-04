b761632c171e8cc904cbfdb79960a12c38e02976
Brav2001 <bacostav@ufpso.edu.co>
2024-01-04 11:40:08 -0500

    cors

--- src/middlewares/cors-middleware.js
+++ src/middlewares/cors-middleware.js
@@ -3,7 +3,11 @@ import cors from "cors";
 const corsMiddleware = () =>
   cors({
     origin: (origin, callback) => {
-      const Accepted_origins = ["http://localhost", "http://localhost:5173"];
+      const Accepted_origins = [
+        "http://localhost",
+        "http://localhost:5173",
+        "https://note-app-brav.vercel.app",
+      ];
 
       if (Accepted_origins.includes(origin)) {
         return callback(null, true);
